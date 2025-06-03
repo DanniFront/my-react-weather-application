4ffc5bebd5e766582c90b80dedfe4827785d0b74
DanniFront <mure.daniela@gmail.com>
2025-06-03 15:53:58 +0200

    Added a footer with links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {" "}
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://www.linkedin.com/in/danielamure/" target="_blank">
+          Daniela Mure
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/DanniFront/my-react-weather-application.git"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
