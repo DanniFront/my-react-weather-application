991e1f53143b1ca2e61fecd90076098b89f57bdb
DanniFront <mure.daniela@gmail.com>
2025-06-03 16:18:51 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
+import React from "react";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       {" "}
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://www.linkedin.com/in/danielamure/" target="_blank">
-          Daniela Mure
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/DanniFront/my-react-weather-application.git"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://www.linkedin.com/in/danielamure/" target="_blank">
+            Daniela Mure
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/DanniFront/my-react-weather-application.git"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
