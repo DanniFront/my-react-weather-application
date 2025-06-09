f885e132850ab2d9f8d035a4d8c1cd509f7b62c3
DanniFront <mure.daniela@gmail.com>
2025-06-09 13:14:58 +0200

    Build the main HTML structure

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import React from "react";
+import Weather from "./Weather.js";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       {" "}
-      <div className="container">
+      <div className="container mt-5">
         <h1>Weather App</h1>
+        <Weather />
         <footer>
           This project was coded by{" "}
-          <a href="https://www.linkedin.com/in/danielamure/" target="_blank">
+          <a
+            href="https://www.linkedin.com/in/danielamure/"
+            target="_blank"
+            rel="noreferrer"
+          >
             Daniela Mure
           </a>{" "}
           and is{" "}
           <a
             href="https://github.com/DanniFront/my-react-weather-application.git"
             target="_blank"
+            rel="noreferrer"
           >
             open-sourced on GitHub
           </a>
